ca14230cf4139e175a1ca2bbbd2004fc7a3b342a
kenielf <chrystian.keni.github@gmail.com>
2025-03-19 18:12:16 -0300

    feat(log): added warning and debug messages

--- config/base/log.sh
+++ config/base/log.sh
@@ -14,10 +14,30 @@ _success() {
     printf -- "\x1b[32m[SUCCESS]\x1b[00m %s\n" "${1}"
 }
 
+## Prints a warning message
+_warn() {
+    if [ -z "${1}" ]; then
+        _error "log: missing warning message"
+    fi
+
+    if [ "${_SHELL_DEBUG}" = "1" ]; then
+        printf -- "\x1b[34m[WARN]\x1b[00m %s\n" "${1}"
+    fi
+
+}
+
 ## Prints an info message
 _info() {
     if [ -z "${1}" ]; then
         _error "log: missing info message"
     fi
     printf -- "\x1b[34m[INFO]\x1b[00m %s\n" "${1}"
+}
+
+## Prints a debug message
+_debug() {
+    if [ -z "${1}" ]; then
+        _error "log: missing debug message"
+    fi
+    printf -- "\x1b[36m[DEBUG]\x1b[00m %s\n" "${1}"
 }
