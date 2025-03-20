7e8b3e55a2aff31d44b4c09ef3bf71066679f537
kenielf <chrystian.keni.github@gmail.com>
2025-03-19 21:13:10 -0300

    feat(log): improved message implementation

--- config/base/log.sh
+++ config/base/log.sh
@@ -1,43 +1,20 @@
-## Prints an error message
-_error() {
+## Prints a message
+_msg() {
     if [ -z "${1}" ]; then
-        error "log: missing error message"
+        _error "log: missing section"
+        return 1
+    elif [ -z "${2}" ]; then
+        _error "log: missing message"
+        return 1
     fi
-    printf -- "\x1b[31m[ERROR]\x1b[00m %s\n" "${1}"
+
+    printf -- "%b %s\n" "${1}" "${2}"
 }
 
-## Prints a success message
-_success() {
-    if [ -z "${1}" ]; then
-        _error "log: missing success message"
-    fi
-    printf -- "\x1b[32m[SUCCESS]\x1b[00m %s\n" "${1}"
-}
+## Types of messages
+alias _error="_msg '\x1b[31m[ERROR]\x1b[00m'"
+alias _success="_msg '\x1b[32m[SUCCESS]\x1b[00m'"
+alias _warn="_msg '\x1b[33m[WARN]\x1b[00m'"
+alias _info="_msg '\x1b[34m[INFO]\x1b[00m'"
+alias _debug="_msg '\x1b[36m[DEBUG]\x1b[00m'"
 
-## Prints a warning message
-_warn() {
-    if [ -z "${1}" ]; then
-        _error "log: missing warning message"
-    fi
-
-    printf -- "\x1b[34m[WARN]\x1b[00m %s\n" "${1}"
-}
-
-## Prints an info message
-_info() {
-    if [ -z "${1}" ]; then
-        _error "log: missing info message"
-    fi
-    printf -- "\x1b[34m[INFO]\x1b[00m %s\n" "${1}"
-}
-
-## Prints a debug message
-_debug() {
-    if [ -z "${1}" ]; then
-        _error "log: missing debug message"
-    fi
-
-    if [ "${_SHELL_DEBUG}" = "1" ]; then
-        printf -- "\x1b[36m[DEBUG]\x1b[00m %s\n" "${1}"
-    fi
-}
