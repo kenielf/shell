c3d461a6ab1c4f2b3a97218c4e309d7c28e7db0e
kenielf <chrystian.keni.github@gmail.com>
2025-03-24 15:07:02 -0300

    feat(python)!: changed the implementation of the dynamic environment

--- config/extra/languages/python.sh
+++ config/extra/languages/python.sh
@@ -1,8 +1,34 @@
 _dependency_add "python3"
 
 ## Python virtual environments
-_python_env_create="python3 -m venv ./.env"
-_python_env_load="source ./.env/bin/activate"
-alias pec="_python_env_create"
-alias pel="_python_env_load"
+pyenv() {
+    # Derive the env directory
+    _env_dir="./.env"
+    if [ -f "${_env_dir}" ]; then
+        # alternative directory, since .env can be an existing file
+        _env_dir="./.venv"
+    fi
+
+    # Source it if it already exists
+    if [ -d "${_env_dir}" ]; then
+        source "${_env_dir}/bin/activate"
+        _info "python: sourced environment (${_env_dir})"
+        return
+    fi
+
+    # Otherwise, create it and source it
+    _debug "python: creating environment"
+    python3 -m venv "${_env_dir}"
+    . "${_env_dir}/bin/activate"
+
+    if [ -f "./requirements.txt" ]; then
+        _debug "python: installing environment dependencies"
+        _cmd="uv"
+        if ! (command -v "${_cmd}" >/dev/null 2>&1); then
+            _cmd "python3 -m"
+        fi
+        ${_cmd} pip install -r "./requirements.txt"
+    fi
+    _info "python: finished setting up environment"
+}
 
