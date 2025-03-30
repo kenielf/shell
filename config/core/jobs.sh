89315e79f587a73faaa66b58e9b3e96757ff88d6
kenielf <chrystian.keni.github@gmail.com>
2025-03-30 18:00:17 -0300

    feat(jobs)!: added a mechanism to blacklist commands

--- config/core/jobs.sh
+++ config/core/jobs.sh
@@ -1,8 +1,11 @@
 _dependency_add "fzf"
 
+## This must be a pipe separated list :D
+JOB_BLACKLIST="zoxide|nvim"
+
 ## Get the amount of jobs as a number (to be used in prompts)
 _jobs_parse_numbers() {
-    _jobs="$(jobs | wc -l)"
+    _jobs="$(jobs | grep -vP "${JOB_BLACKLIST}" | wc -l)"
     if [ "${_jobs}" -ge 1 ]; then
         echo " (${_jobs})"
     else
