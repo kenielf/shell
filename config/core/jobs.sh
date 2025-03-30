e16c23f2c2b98f9b938a6a36f3fb507a575f71f0
kenielf <chrystian.keni.github@gmail.com>
2025-03-30 18:10:27 -0300

    feat(jobs): implemented fgi and bgi, also removed nvim from the default blocklist

--- config/core/jobs.sh
+++ config/core/jobs.sh
@@ -1,7 +1,7 @@
 _dependency_add "fzf"
 
 ## This must be a pipe separated list :D
-JOB_BLACKLIST="zoxide|nvim"
+JOB_BLACKLIST="zoxide"
 
 ## Get the amount of jobs as a number (to be used in prompts)
 _jobs_parse_numbers() {
@@ -14,7 +14,25 @@ _jobs_parse_numbers() {
 }
 
 ## Move process to foreground interactively using fzf
-# fgi() {}
+fgi() {
+    # Find all jobs that aren't done
+    _jobs="$(jobs | grep -v 'Done' | wc -l)"
+
+    if [ "${_jobs}" -ge 1 ]; then
+        # Get job number and foreground it
+        _chosen="$(jobs | fzf | grep -Po '\[[0-9]+\]' | tr -d '[]')"
+        fg %${_chosen}
+    fi
+}
 
 ## Move process to background interactively using fzf
-# bgi() {}
+bgi() {
+    # Find all jobs that aren't done
+    _jobs="$(jobs | grep -v 'Done' | wc -l)"
+
+    if [ "${_jobs}" -ge 1 ]; then
+        # Get job number and foreground it
+        _chosen="$(jobs | fzf | grep -Po '\[[0-9]+\]' | tr -d '[]')"
+        bg %${_chosen}
+    fi
+}
