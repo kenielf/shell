58928d971b4e9a9e3e373c09c310f39b717825bf
kenielf <chrystian.keni.github@gmail.com>
2025-05-27 10:02:35 -0300

    feat(navigation): added pwx - working directory to clipboard

--- config/core/navigation.sh
+++ config/core/navigation.sh
@@ -1,4 +1,7 @@
-_dependency_add "zoxide tree eza vivid"
+_dependency_add "xclip zoxide tree eza vivid"
+
+# Send working directory to clipboard
+alias pwx="pwd | xclip -sel c"
 
 ## Creates a directory if needed and accesses it
 mkcd() {
