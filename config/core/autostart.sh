86df431a8fc9d309ba724d8a702b06cf33c0ada3
kenielf <chrystian.keni.github@gmail.com>
2025-07-14 15:53:26 -0300

    refac(autostart): use kscfg namespace for the autostart commands.

--- config/core/autostart.sh
+++ config/core/autostart.sh
@@ -1,16 +1,16 @@
-_AUTOSTART_COMMANDS=""
+_KSCFG_AUTOSTART_COMMANDS=""
 
 ## Adds functions (not full commands!) to be automatically executed on startup
-_autostart_add() {
+_kscfg_autostart_add() {
     if [ -z "${1}" ]; then
         _error "autostart: command to be autostarted is missing"
     fi
-    _AUTOSTART_COMMANDS="$(_append "${_AUTOSTART_COMMANDS}" "${1}")"
+    _KSCFG_AUTOSTART_COMMANDS="$(_append "${_KSCFG_AUTOSTART_COMMANDS}" "${1}")"
 }
 
 ## Run all commands in autostart
-_autostart_run() {
-    for func in ${_AUTOSTART_COMMANDS}; do
+_kscfg_autostart_run() {
+    for func in ${_KSCFG_AUTOSTART_COMMANDS}; do
         _debug "autostart: running '${func}'"
         "${func}"
     done
