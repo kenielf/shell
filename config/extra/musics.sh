3037a696d2f6222f01dbd7b98f92c7588f0b4499
kenielf <chrystian.keni.github@gmail.com>
2025-07-24 07:10:21 -0300

    fix(musics)!: dlmp3 function was missing the actual parameter.

--- config/extra/musics.sh
+++ config/extra/musics.sh
@@ -10,5 +10,5 @@ dlmp3() {
     yt-dlp -x \
         -f bestaudio/best --embed-thumbnail --audio-format mp3 --add-metadata \
         --downloader aria2c --downloader-args="'aria2c:${_ARIA2C_ARGS}'" \
-        -o '%(title)s.%(ext)s'
+        -o '%(title)s.%(ext)s' "${1}"
 }
