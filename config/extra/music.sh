75018b178367b9d7718e337b97168fa6affbb839
kenielf <chrystian.keni.github@gmail.com>
2026-01-25 02:37:30 -0300

    fix(music): fixed wrong implementation for aria2c args

--- config/extra/music.sh
+++ config/extra/music.sh
@@ -1,5 +1,7 @@
 _kscfg_dependency_add "yt-dlp"
 
+export _ARIA2C_ARGS="--file-allocation=falloc"
+
 ## Downloads a video at its highest found resolution
 dlmp3() {
     if [ -z "${1}" ]; then
@@ -9,6 +11,6 @@ dlmp3() {
 
     yt-dlp -x \
         -f bestaudio/best --embed-thumbnail --audio-format mp3 --add-metadata \
-        --downloader aria2c --downloader-args="'aria2c:${_ARIA2C_ARGS}'" \
+        --downloader "aria2c" --downloader-args="aria2c:${_ARIA2C_ARGS}" \
         -o '%(title)s.%(ext)s' "${1}"
 }
